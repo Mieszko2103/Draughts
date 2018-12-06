b93634f76883efea39ea9207511ad977fb43e187
Mieszko <mieszko2103@gmail.com>
2018-12-06 22:07:04 +0100

    Attack Function

--- src/models/Figure.java
+++ src/models/Figure.java
@@ -26,19 +26,24 @@ public class Figure {
 
 
     public boolean move(Field moveTo) {
-        if (moveTo.getOccupatedBy() != null || !moveTo.isActive()) {
+        if (moveTo.getOccupatedBy() != null && !moveTo.isActive()) {
             System.out.println("Wrong move coordinates");
             return false;
         } else if (isWhite) {
-            if ((moveTo.getColumn() == (courrentField.getColumn() + 1)) && (moveTo.getRow() == (courrentField.getRow() - 1)) ||
-                    (moveTo.getColumn() == courrentField.getColumn() - 1) && (moveTo.getRow() == courrentField.getRow() - 1)) {
+            if ((moveTo.getColumn() == (courrentField.getColumn() + 1)) &&
+                    (moveTo.getRow() == (courrentField.getRow() - 1)) ||
+                    (moveTo.getColumn() == courrentField.getColumn() - 1) &&
+                            (moveTo.getRow() == courrentField.getRow() - 1)) {
                 courrentField.setOccupatedBy(null);
                 moveTo.setOccupatedBy(this);
                 this.courrentField = moveTo;
                 return true;
             } else return false;
-        } else if ((moveTo.getColumn() == courrentField.getColumn() + 1) && (moveTo.getRow() == courrentField.getRow() + 1) ||
-                (moveTo.getColumn() == courrentField.getColumn() - 1) && (moveTo.getRow() == courrentField.getRow() + 1)) {
+
+        } else if (((moveTo.getColumn() == courrentField.getColumn() + 1) &&
+                (moveTo.getRow() == courrentField.getRow() + 1)) ||
+                ((moveTo.getColumn() == courrentField.getColumn() - 1) &&
+                        (moveTo.getRow() == courrentField.getRow() + 1))) {
 
             courrentField.setOccupatedBy(null);
             moveTo.setOccupatedBy(this);
@@ -46,6 +51,26 @@ public class Figure {
             return true;
         }
         return false;
+    }
+
+    public boolean attack(Field moveTo, Field attackOver) {
+        if (moveTo.getOccupatedBy() != null ||
+                !moveTo.isActive() ||
+                attackOver.getOccupatedBy() != null &&
+                attackOver.getOccupatedBy().isWhite == this.isWhite) {
+            System.out.println("You can't attack like that");
+            return false;
+        } else if (((moveTo.getColumn() == (courrentField.getColumn() + 2)) && (moveTo.getRow() == (courrentField.getRow() - 2))) ||
+                ((moveTo.getColumn() == (courrentField.getColumn() + 2)) && (moveTo.getRow() == (courrentField.getRow() + 2))) ||
+                ((moveTo.getColumn() == (courrentField.getColumn() - 2)) && (moveTo.getRow() == (courrentField.getRow() - 2))) ||
+                ((moveTo.getColumn() == (courrentField.getColumn() - 2)) && (moveTo.getRow() == (courrentField.getRow() + 2)))) {
+            courrentField.setOccupatedBy(null);
+            attackOver.setOccupatedBy(null);
+            moveTo.setOccupatedBy(this);
+            this.courrentField = moveTo;
+            return true;
+        } return false;
+
     }
 
 }
--- src/play/Main.java
+++ src/play/Main.java
@@ -8,12 +8,22 @@ public class Main {
         Board board = new Board();
         board.show();
 
-//        board.getField(3,3).getOccupatedBy().setIcon("[G]");
-//        System.out.println(board.getField(3,3).getOccupatedBy().move(board.getField(4,2)));
-//
-//        board.show();
+        System.out.println(board.getField(3,3).getOccupatedBy().move(board.getField(4,2)));
 
+        board.show();
 
+        System.out.println(board.getField(4,2).getOccupatedBy().move(board.getField(5,3)));
+
+        board.show();
+
+        System.out.println(board.getField(6,2).getOccupatedBy().attack(board.getField(4,4),board.getField(5,3)));
+        board.show();
+
+        System.out.println(board.getField(3,5).getOccupatedBy().attack(board.getField(5,3),board.getField(4,4)));
+        board.show();
+
+        System.out.println(board.getField(7,3).getOccupatedBy().attack(board.getField(5, 5),board.getField(6,4)));
+        board.show();
         //System.out.println(board.getField(6,0).getOccupatedBy().move(board.getField(5,1)));
         // board.show();
     }
